ce4ec7a4ec35eb5ebd3748c4452e9d7bf901818f
Michael Hoehn <michael.hoehn@burohappold.com>
2022-05-26 07:32:38 -0600

    Update CSS in template files

--- fxhash/subStruct/public/sketch.js
+++ fxhash/subStruct/public/sketch.js
@@ -3,18 +3,23 @@ let x = 10;
 let y;
 
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(1000, 1000);
     y = height / 2;
+    background(250);
 }
 
 function draw() {
     segments = 5;
-    background(250);
+
     stroke(0);
     strokeWeight(5);
 
     for (let i = 0; i < segments; i++) {
-        point(x, y);
-        x = x + i; 
+        line(0, y, x, y);
+        x = x + i;
+        if (x > width || x < 0) {
+            x = x + 5;
+            y = y + 5;
+        }
     }
 }
