653cd2febfbe82b614a0725696cb460fcce4f005
Michael Hoehn <michael.hoehn@burohappold.com>
2022-06-15 22:36:03 -0600

    trim it and add css + fxhash snippet script

--- Threejs/fxhash_Threejs_boilerplate/js/index.js
+++ Threejs/fxhash_Threejs_boilerplate/js/index.js
@@ -5,7 +5,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const geometry = new THREE.BoxGeometry( fxrand(), fxrand(), fxrand() );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
